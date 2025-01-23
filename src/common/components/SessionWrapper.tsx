99a9e44a1f8105f74b5ceadd1a5f534f6c99f7d0
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-23 12:25:34 +0530

    ntho matti

--- src/common/components/SessionWrapper.tsx
+++ src/common/components/SessionWrapper.tsx
@@ -7,9 +7,10 @@ const SessionWrapper = ({ children }: { children: React.ReactNode }) => {
 
   if (status === "loading") {
     return <div>Loading...</div>;
-  } else if (status === "unauthenticated") {
-    return <div>Unauthenticated</div>;
   }
+  //  else if (status === "unauthenticated") {
+  //   return <div>Unauthenticated</div>;
+  // }
 
   return <>{children}</>;
 };
