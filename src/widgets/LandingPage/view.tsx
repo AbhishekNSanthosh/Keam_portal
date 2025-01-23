9bac619b8f3e02a44ccb77061b7907f6689c2825
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-23 07:24:44 +0530

    feat:login page

--- src/widgets/LandingPage/view.tsx
+++ src/widgets/LandingPage/view.tsx
@@ -1,7 +1,12 @@
+import Header from '@widgets/Header'
 import React from 'react'
+import Content from './components/Content'
 
 export default function LandingPageView() {
   return (
-    <div>view</div>
+    <main>
+      <Header/>
+      <Content/>
+    </main>
   )
 }
