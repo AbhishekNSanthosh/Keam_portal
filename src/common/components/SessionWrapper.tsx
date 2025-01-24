49c2b216c1398f89d5abd2dd9e220fa49f32c944
Joeljoy1237 <joeljoy1237@gmail.com>
2025-01-24 11:05:40 +0530

    fix :session redirect

--- src/common/components/SessionWrapper.tsx
+++ src/common/components/SessionWrapper.tsx
@@ -10,14 +10,17 @@ const SessionWrapper = ({
   admin?: boolean;
   children: React.ReactNode;
 }) => {
-  const { data: session, status } = useSession();
+  const { data: session, status } = useSession() as {
+    data: { user: { isAdmin: boolean } } | null;
+    status: string;
+  };
   const router = useRouter();
 
   useEffect(() => {
     const checkSession = async () => {
       if (status === "unauthenticated") {
         await new Promise((resolve) => setTimeout(resolve, 1000)); // 1-second delay;
-        if (admin) {
+        if (admin && !session?.user.isAdmin) {
           router.push("/admin/login");
         } else {
           router.push("/login");
