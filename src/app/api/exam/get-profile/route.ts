9cec5e455d2b0d410f30d026748486189df50afd
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-24 11:45:04 +0530

    refactor:

--- src/app/api/exam/get-profile/route.ts
+++ src/app/api/exam/get-profile/route.ts
@@ -5,8 +5,8 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (req: NextRequest) => {
     try {
         // Parse the request body to get the user ID
-        const { id } = await req.json();
-        if (!id) {
+        const { userId } = await req.json();
+        if (!userId) {
             return NextResponse.json(
                 { message: "User ID is required" },
                 { status: 400 }
@@ -17,7 +17,7 @@ export const POST = async (req: NextRequest) => {
         await connectToDB();
 
         // Fetch the user details by ID
-        const user = await User.findById(id);
+        const user = await User.findById({ _id: userId });
         if (!user) {
             return NextResponse.json(
                 { message: "User not found" },
@@ -26,7 +26,7 @@ export const POST = async (req: NextRequest) => {
         }
 
         // Remove sensitive data (e.g., password) from the user object
-        const { score, ...sanitizedUser } = user._doc;
+        const { score,isAdmin,isAttempted,isSubmitted,__v, ...sanitizedUser } = user._doc;
 
         // Respond with the user details
         return NextResponse.json(
@@ -34,7 +34,7 @@ export const POST = async (req: NextRequest) => {
             { status: 200 }
         );
     } catch (err) {
-        console.error("Error fetching user details:", err);
+        console.log("Error fetching user details:", err);
         return NextResponse.json(
             { message: "Internal Server Error" },
             { status: 500 }
