8589b08dd0eccadc22db6a4769bafd5c9842dbf8
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-24 20:52:44 +0530

    fix

--- src/common/utils/database.ts
+++ src/common/utils/database.ts
@@ -4,31 +4,19 @@ let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
-  
   if (isConnected) {
     console.log("==> Mongo Already connected");
     return;
   }
 
   try {
-    // Ensure the environment variable name is correct
-    const mongoURI = process.env.MANGODB_URL;  // Update to the correct env variable
-    
-    if (!mongoURI) {
-      throw new Error("MongoDB URI is not defined in the environment variables.");
-    }
-
-    await mongoose.connect(mongoURI, {
-      dbName: "keamportal", // Database name
-      writeConcern: { w: 'majority' }, // Ensure data consistency
+    await mongoose.connect(process.env.MONGODB_URL !, {
+      dbName: "keamportal",
+      writeConcern: { w: 'majority' },
     });
-
     isConnected = true;
     console.log("==> Mongo Successfully connected");
-
   } catch (error) {
-    console.error('Connection error:', error);
-    // You can throw an error if you want to stop the process in case of a connection issue
-    // throw error;
+    console.log('Connection error:', error);
   }
 };
