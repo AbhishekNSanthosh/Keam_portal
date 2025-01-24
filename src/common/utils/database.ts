5a4863a753f47f3365bc98e3c5a572fdbe0a2516
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-24 21:16:25 +0530

    feat:

--- src/common/utils/database.ts
+++ src/common/utils/database.ts
@@ -4,19 +4,25 @@ let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
+  
   if (isConnected) {
     console.log("==> Mongo Already connected");
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL !, {
+    if (!process.env.MONGODB_URL) {
+      throw new Error("MONGODB_URL is not defined in the environment variables.");
+    }
+    
+    await mongoose.connect(process.env.MONGODB_URL, {
       dbName: "keamportal",
-      writeConcern: { w: 'majority' },
+      writeConcern: { w: "majority" },
     });
     isConnected = true;
     console.log("==> Mongo Successfully connected");
-  } catch (error) {
-    console.log('Connection error:', error);
+  } catch (error:any) {
+    console.error("==> Connection error:", error.message);
+    throw error;
   }
 };
